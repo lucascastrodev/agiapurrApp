793f6b5568fdd0c1becf600a9ea0b52d3c55aa1f
lucascastrodev <lucastro1991.lc@gmail.com>
2026-03-19 20:57:02 -0300

    Se agrego una nueva clase ProductoProveedor.cs para poder crear la seccion para los pedidos a los proveedores. En la Base de Datos ya se creo la tabla PRODUCTOS_PROVEEDOR

---
+++ Dominio/ProductoProveedor.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dominio
+{
+    public class ProductoProveedor
+    {
+        public int Id { get; set; }
+
+        // Relación con el Proveedor
+        public Proveedor Proveedor { get; set; }
+
+        public string Codigo { get; set; }
+
+        // El nombre específico que usa el proveedor en su lista de precios
+        public string Descripcion { get; set; }
+
+        // Relación con la Marca
+        public Marca Marca { get; set; }
+
+        public decimal PrecioUnitario { get; set; }
+
+        public bool Estado { get; set; }
+
+        // Constructor para inicializar los objetos anidados y evitar NullReferenceExceptions
+        public ProductoProveedor()
+        {
+            Proveedor = new Proveedor();
+            Marca = new Marca();
+        }
+    }
+}
